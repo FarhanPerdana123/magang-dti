4fcc616f6a83eedf89785d4d57c8b55b5ce6ab50
wildanhanifabdillah <wildanhanifabdillah27@gmail.com>
2026-05-25 15:01:53 +0700

    remove debug files from tracking

--- check-blocks.php
+++
@@ -1,7 +0,0 @@
-﻿git : fatal: path 'check-blocks.php' exists on disk, but not in '7248985'
-At line:3 char:1
-+ git show 7248985:check-blocks.php 2>&1 | Out-File "check-blocks.php"  ...
-+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    + CategoryInfo          : NotSpecified: (fatal: path 'ch...ot in '7248985':String) [], RemoteException
-    + FullyQualifiedErrorId : NativeCommandError
- 
