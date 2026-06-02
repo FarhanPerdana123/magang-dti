ec89cebbfec74ed7117cc60c755520f1705a5ef7
wildanhanifabdillah <wildanhanifabdillah27@gmail.com>
2026-06-02 08:07:07 +0700

    Update typography and add Google Fonts for improved styling

--- wp-content/themes/ugm-faculty/inc/enqueue.php
+++ wp-content/themes/ugm-faculty/inc/enqueue.php
@@ -42,6 +42,13 @@ function ugm_enqueue_assets() {
 		'5.3.3'
 	);
 
+	wp_enqueue_style(
+		'ugm-google-fonts',
+		'https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,600;1,700&family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600;1,700&display=swap',
+		array(),
+		null
+	);
+
 	wp_enqueue_script(
 		'bootstrap-bundle',
 		'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
